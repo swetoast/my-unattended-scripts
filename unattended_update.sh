c02f3a67cc8ab131f3c292297c57c2ea10c9e943
Toast <swetoast@users.noreply.github.com>
2021-07-04 21:04:32 +0200

    Create unattended_update.sh

---
+++ unattended_update.sh
@@ -0,0 +1,187 @@
+#!/bin/sh
+export PATH=/home/toast/.cargo/env:$PATH
+config=/opt/etc/unattended_update.conf
+
+if [ "$(id -u)" != "0" ]; then exec /usr/bin/sudo /bin/sh "$0"; fi
+
+if [ -f $config ]
+  then echo "Configuration file found at $config"
+       echo "Updating Repository lists."
+  else echo "No configuration file present at $config"
+       exit 0
+fi
+
+. $config
+
+setting_debug_enabled () { set -x; }
+setting_debug_disable () { set +x; }
+
+if [ $set_debug = "enabled" ]; then setting_debug_enabled; fi
+
+check_online () {
+while ! ping -q -c 1 google.com >/dev/null 2>&1; do
+  sleep 1
+  WaitSeconds=$((WaitSeconds+1))
+  [ $WaitSeconds -gt 300 ] && echo "Not Online"; exit 1
+done
+}
+
+pushover_message () {
+curl -s \
+  --form-string "token=$pushover_token" \
+  --form-string "user=$pushover_username" \
+  --form-string "message=The following device will be updated: $(cat /etc/hostname), $count pending packages will be updated here is the package list $packagelist" \
+    https://api.pushover.net/1/messages.json
+}
+
+pushbullet_message () {
+  message="$count pending $type packages will be updated here is the package list $packagelist"
+  title="The following device will be updated: $(cat /etc/hostname)"
+  curl -u $pushbullet_token: https://api.pushbullet.com/v2/pushes -d type=note -d title="$title" -d body="$message"
+}
+
+check_packages () {
+if [ "$count" -eq "0" ]
+  then echo "No available updates on the following device: $(echo $HOSTNAME)"
+  else echo "$count available updates on the following device: $(echo $HOSTNAME)\nHere is the package list\n $packagelist"
+    if [ $(which kodi-send | wc -l) -eq 1 ]; then kodi-send --action="Notification($count available updates, Here is the packagelist: $packagelist,$timer)"; fi
+    if [ $use_pushbullet = "enabled" ]; then pushbullet_message; fi
+    if [ $use_pushover = "enabled" ]; then pushover_message; fi
+fi }
+
+apt_list_packages () {
+  type=debian
+  packagelist=$(apt list --upgradable | cut -d' ' -f1-2)
+  pkglist=$(apt-get -su --assume-yes dist-upgrade)
+  pending=$(echo "$pkglist" | grep -oE "[0-9]+ upgraded, [0-9]+ newly installed, [0-9]+ to remove and [0-9]+ not upgraded\.")
+  upgraded=$(echo "$pending" | grep -oE "[0-9]+ upgraded" | cut -d' ' -f1)
+  installed=$(echo "$pending" | grep -oE "[0-9]+ newly installed" | cut -d' ' -f1)
+  removed=$(echo "$pending" | grep -oE "[0-9]+ to remove" | cut -d' ' -f1)
+  count=$(( $upgraded + $installed + $removed ))
+}
+
+snap_list_packages () {
+  type=snap
+  packagelist=$(snap refresh --list)
+  pending=$(echo "$(snap refresh --list) updates available")
+  count=$(snap refresh --list | wc -l)
+}
+
+pacman_list_packages () {
+  type=arch
+  packagelist=$(pacman -Qu)
+  pending=$(echo "$(pacman -Qu | wc -l) updates available")
+  count=$(pacman -Qu | wc -l)
+}
+
+npm_list_packages () {
+  type=nodejs
+  packagelist=$(npm outdated)
+  pending=$(echo "$(npm outdated | wc -l) updates available")
+  count=$(npm outdated | wc -l)
+}
+
+opkg_list_packages () {
+  type=opkg
+  packagelist=$(opkg list-upgradable)
+  pending=$(echo "$(opkg list-upgradable | wc -l) updates avalable")
+  count=$(opkg list-upgradable | wc -l)
+}
+
+pip_list_packages () {
+  type=python
+  pending=$(echo "$(pip list --outdated --format=freeze | wc -l) oudated python packages")
+  packagelist=$(pip list --outdated --format=freeze)
+  count=$(pip list --outdated --format=freeze | wc -l)
+}
+
+pacman_upgrader () {
+pacman -Sy
+pacman_list_packages
+check_packages
+if [ "$count" -ge "1" ]; then
+  yes | pacman -Suy --overwrite="*"
+  yes | pacman -Sc
+fi
+}
+
+apt_upgrader () {
+  /etc/init.d/rpimonitor update
+  apt_list_packages
+  check_packages
+  if [ "$count" -ge "1" ]
+    then apt-get dist-upgrade -qq -y --assume-yes
+         apt-get autoremove -qq -y
+         apt-get autoclean -qq -y
+         apt-get -qq -y purge $(/usr/bin/dpkg -l | /bin/grep "^rc" | /usr/bin/awk '{print $2}')
+  fi
+  if [ $(cat /proc/version | grep -oE "osmc" | wc -l) -ge 1 ]; then kodi_upgrader; fi
+}
+
+ipkg_upgrader () {
+  ipkg upgrade
+}
+
+kodi_upgrader () {
+  if systemctl is-enabled mediacenter.service >/dev/null 2>&1 && ! systemctl is-active mediacenter.service >/dev/null 2>&1
+           then systemctl restart mediacenter.service; fi
+  sleep 60
+  kodi-send \
+      --action="Notification(No updates available, Checking for updates in Kodi Repository,$timer)" \
+      --action="UpdateAddonRepos" \
+      --action="UpdateLocalAddons"
+}
+
+nodejs_upgrader () {
+  npm_list_packages
+  check_packages
+  if [ "$count" -ge "1" ]
+    then npm update -g
+  fi }
+
+opkg_upgrader () {
+  PATH=/opt/bin/go/bin:/sbin:/usr/sbin:/bin:/usr/bin:/usr/builtin/sbin:/usr/builtin/bin:/usr/local/sbin:/usr/local/bin:/opt/sbin:/opt/bin
+  opkg_list_packages
+  check_packages
+  if [ "$count" -ge "1" ]
+    then opkg upgrade
+  fi }
+
+rust_upgrader () {
+su - $myusername -c "rustup update"
+su - $myusername -c "rustup self update"
+su - $myusername -c "cargo install-update -a"
+su - $myusername -c "cargo-cache -ae"
+}
+
+python_upgrader () {
+  pip_list_packages
+  check_packages
+  if [ "$count" -ge "1" ]
+    then pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install --exists-action s -U
+  fi }
+
+snap_upgrader () {
+snap_list_packages
+snap refresh
+set -eu
+LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
+}
+
+detect_updater () {
+  if [ $(which ipkg | wc -l) -eq 1 ]; then ipkg update; ipkg_upgrader; fi
+  if [ $(which opkg | wc -l) -eq 1 ]; then opkg update; opkg_upgrader; fi
+  if [ $(which apt | wc -l) -eq 1 ]; then apt-get update; apt_upgrader; fi
+  if [ $(which pacman | wc -l) -eq 1 ]; then pacman_upgrader; fi
+  #if [ $(which pip | wc -l) -eq 1 ]; then python_upgrader; fi
+  if [ $(which npm | wc -l) -eq 1 ]; then nodejs_upgrader; fi
+  if [ $(su - $myusername -c "which rustup" | wc -l) -eq 1 ]; then rust_upgrader; fi
+}
+
+check_online
+detect_updater
+
+if [ $set_debug = "enabled" ]; then setting_debug_disable; fi
