b1bc60c66166fcfd41ba562d638f63ead378e7cc
Toast <swetoast@users.noreply.github.com>
2023-08-13 19:35:46 +0200

    Update raspberry_throttled.sh

--- SOC/raspberry_throttled.sh
+++ SOC/raspberry_throttled.sh
@@ -15,9 +15,7 @@ throttled=$(vcgencmd get_throttled | cut -d "=" -f 2)
 get_throttled=$(vcgencmd get_throttled | cut -d "=" -f 2 | cut -d "x" -f 2)
 
 pushbullet_message () {
-
 curl -u "$pushbullet_token": https://api.pushbullet.com/v2/pushes -d type=note -d title="Throttling Detected on $(cat /etc/hostname)" -d body="Throttling Detected on $(cat /etc/hostname) Status code: $throttled"
-
 }
 
 if [ "$get_throttled" -ne 0 ]; then pushbullet_message; fi
