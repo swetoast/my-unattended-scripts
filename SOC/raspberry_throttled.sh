62aae9ded1eb393ba13b97052ff50347c7a23621
Toast <swetoast@users.noreply.github.com>
2023-08-25 13:21:50 +0200

    Update raspberry_throttled.sh

--- SOC/raspberry_throttled.sh
+++ SOC/raspberry_throttled.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Rev 1
+# Rev 2
 
 CONFIG=/opt/etc/unattended_update.conf
 
@@ -16,6 +16,8 @@ setting_debug_disable () { set +x; }
 if [ "$set_debug" = "enabled" ]; then setting_debug_enabled; fi
 
 get_throttled=$(vcgencmd get_throttled | cut -d "=" -f 2)
+get_temperature=$(vcgencmd measure_temp | cut -d "=" -f 2)
+get_voltage=$(vcgencmd measure_volts | cut -d "=" -f 2)
 
 pushbullet_message () {
 curl -u "$pushbullet_token": https://api.pushbullet.com/v2/pushes -d type=note -d title="Throttling Detected on $(cat /etc/hostname)" -d body="Current status is $current"
@@ -23,10 +25,10 @@ curl -u "$pushbullet_token": https://api.pushbullet.com/v2/pushes -d type=note -
 
 check_throttled () {
 case $get_throttled in
-0x10000) current=$(echo "Under-voltage has occurred") pushbullet_message ;;
+0x10000) current=$(echo "Under-voltage has occurred, current voltage is $get_voltage") pushbullet_message ;;
 0x20000) current=$(echo "Arm frequency capping has occurred") pushbullet_message ;;
 0x40000) current=$(echo "Throttling has occurred") pushbullet_message ;;
-0x80000) current=$(echo "Soft temperature limit has occurred") pushbullet_message ;;
+0x80000) current=$(echo "Soft temperature limit has occurred, current tempature is $get_temperature") pushbullet_message ;;
 esac
 }
 
