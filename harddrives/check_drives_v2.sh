5c0218438ad518373a6ee717d80b2cad439a4a9b
Toast <swetoast@users.noreply.github.com>
2024-07-23 13:13:13 +0200

    Update check_drives_v2.sh

--- harddrives/check_drives_v2.sh
+++ harddrives/check_drives_v2.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
 # Revision 15
 # Configuration
+
 CONFIG=/opt/etc/unattended_update.conf
 
-# Ensure the script is run as root
 if [ "$(id -u)" != "0" ]; then exec sudo /bin/bash "$0"; fi
 
-# Check if configuration file exists
 if [ ! -f "$CONFIG" ]; then
     echo "No configuration file present at $CONFIG"
     exit 0
 fi
 
-# Source the configuration file
 . "$CONFIG"
 
-# Function to toggle debug setting
 [ "$set_debug" = "enabled" ] && set -x || set +x
 
-# Function to send a message
 send_message() {
     local event=$1
     local body=$2
@@ -26,118 +22,181 @@ send_message() {
     curl -u "$pushbullet_token": https://api.pushbullet.com/v2/pushes -d type=note -d title="$title" -d body="$body"
 }
 
-# Function to send a file
 send_file() {
     local file_path=$1
-    local file_name=$(basename "$file_path")
+    local file_name
+    file_name=$(basename "$file_path")
     local title="Here is a log file from HDD Scans on $HOSTNAME"
 
-    # Check if the file size is less than the max size
-    if [ $(stat -c%s "$file_path") -le $MAX_SIZE ]; then
-        curl -u "$pushbullet_token": https://api.pushbullet.com/v2/pushes -X POST -F type=file -F file_name="$file_name" -F file=@$file_path -F title="$title"
+    if [ $(stat -c%s "$file_path") -le "$MAX_SIZE" ]; then
+        curl -u "$pushbullet_token": https://api.pushbullet.com/v2/pushes -X POST -F type=file -F file_name="$file_name" -F file=@"$file_path" -F title="$title"
     else
         send_message "File Compression" "File $file_name is larger than 25MB. Attempting to compress..."
-
-        # Compress the file
         gzip -c "$file_path" > "$file_path.gz"
-
-        # Check if the compressed file size is less than the max size
-        if [ $(stat -c%s "$file_path.gz") -le $MAX_SIZE ]; then
-            curl -u "$pushbullet_token": https://api.pushbullet.com/v2/pushes -X POST -F type=file -F file_name="$file_name.gz" -F file=@$file_path.gz -F title="$title"
+        if [ $(stat -c%s "$file_path.gz") -le "$MAX_SIZE" ]; then
+            curl -u "$pushbullet_token": https://api.pushbullet.com/v2/pushes -X POST -F type=file -F file_name="$file_name.gz" -F file=@"$file_path".gz -F title="$title"
         else
-            # Send a Pushbullet message indicating that the file is too large
             send_message "File Compression" "Compressed file $file_name.gz is still larger than 25MB and will not be sent."
         fi
     fi
 }
 
-# Function to perform badblocks check
-function badblocks_check {
-    send_message "Badblocks Check" "Performing badblocks check on $1..."
-    badblocks -v $1
-}
-
-# Function to perform SMART test
-function smart_test {
-    send_message "SMART Test" "Performing SMART test on $1..."
-    if [[ $1 == /dev/nvme* ]]; then
-        nvme smart-log $1
+badblocks_check() {
+    local disk=$1
+    local badblocks_file="/var/log/badblocks-$disk.log"
+    send_message "Badblocks Check" "Performing badblocks check on $disk..."
+    badblocks -v -s -o "$badblocks_file" "$disk"
+    if [ -s "$badblocks_file" ]; then
+        send_message "Badblocks Check" "Badblocks found on $disk. Sending log file..."
+        send_file "$badblocks_file"
     else
-        smartctl -t long $1
+        send_message "Badblocks Check" "No badblocks found on $disk."
     fi
 }
 
-# Function to check partition errors on SD cards
-function fsck_check {
-    send_message "Partition Error Check" "Checking partition errors on $1..."
-    fsck -n $1
+smart_test() {
+    local disk=$1
+    local smart_log_file="/var/log/smart-$disk.log"
+    send_message "SMART Test" "Performing SMART test on $disk..."
+    if [[ $disk == /dev/nvme* ]]; then
+        nvme smart-log "$disk" > "$smart_log_file"
+    else
+        smartctl -t long "$disk" > "$smart_log_file"
+    fi
+    if [ -s "$smart_log_file" ]; then
+        send_message "SMART Test" "SMART test completed on $disk. Sending log file..."
+        send_file "$smart_log_file"
+    else
+        send_message "SMART Test" "No SMART data found on $disk."
+    fi
 }
 
-# Function to clean system logs
+fsck_check() {
+    local fs_type=$1
+    local disk=$2
+    local fsck_log_file="/var/log/fsck-$disk.log"
+    # Define the filesystem types you want to allow for fsck_check
+    allowed_fs_types=("ext4" "xfs" "btrfs" "vfat" "zfs")
+    if [[ " ${allowed_fs_types[*]} " =~ ${fs_type} ]]; then
+        send_message "Partition Error Check" "Checking partition errors on $disk of type $fs_type..."
+        case $fs_type in
+            ext4)
+                ext4_fsck "$disk" > "$fsck_log_file"
+                ;;
+            xfs)
+                xfs_check "$disk" > "$fsck_log_file"
+                ;;
+            btrfs)
+                check_btrfs "$disk" > "$fsck_log_file"
+                ;;
+            vfat)
+                dosfsck -a "$disk" > "$fsck_log_file"
+                ;;
+            zfs)
+                zfs_maintenance "$disk" > "$fsck_log_file"
+                ;;
+            *)
+                echo "Unsupported filesystem type: $fs_type"
+                ;;
+        esac
+        if [ -s "$fsck_log_file" ]; then
+            send_message "Partition Error Check" "Filesystem check completed on $disk. Sending log file..."
+            send_file "$fsck_log_file"
+        else
+            send_message "Partition Error Check" "No filesystem errors found on $disk."
+        fi
+    else
+        echo "Skipping fsck_check on disk $disk with unsupported filesystem type: $fs_type"
+    fi
+}
+
+fs_maintenance() {
+    local fs_type=$1
+    local disk=$2
+    # Define the filesystem types you want to allow for fs_maintenance
+    allowed_fs_types=("ext4" "btrfs" "zfs")
+    if [[ " ${allowed_fs_types[*]} " =~ ${fs_type} ]]; then
+        send_message "Filesystem Maintenance" "Performing maintenance on $disk of type $fs_type..."
+        case $fs_type in
+            ext4)
+                trim_ext4 "$disk"
+                ;;
+            btrfs)
+                btrfs_balance "$disk"
+                ;;
+            zfs)
+                zfs_maintenance "$disk"
+                ;;
+            *)
+                echo "Unsupported filesystem type for maintenance: $fs_type"
+                ;;
+        esac
+    else
+        echo "Skipping fs_maintenance on disk $disk with unsupported filesystem type: $fs_type"
+    fi
+}
+
 clean_system_logs() {
     send_message "System Log Cleanup" "Cleaning system logs..."
     journalctl --rotate --vacuum-size=1M
 }
 
-# Function to trim ext4 filesystems and scrub btrfs filesystems
-function check_filesystem {
-    send_message "Filesystem Check" "Trimming ext4 filesystems and starting btrfs scrub..."
-    df -T | awk '/btrfs/ {system("btrfs scrub start "$7)}'
-    df -T | awk '/ext4/ {system("fstrim -Av")}'
+check_btrfs() {
+    send_message "Btrfs Check" "Starting btrfs scrub..."
+    btrfs scrub start "$1"
 }
 
-# Function to perform checks on disks
-function perform_checks {
-    local disk_type=$1
-    local disk_path=$2
-    local check_function=$3
+btrfs_balance() {
+    send_message "Btrfs Balance" "Performing btrfs balance..."
+    btrfs balance start -dusage=50 "$1"
+}
 
-    for disk in $disk_path; do
-        $check_function $disk
+trim_ext4() {
+    send_message "Ext4 Trim" "Trimming ext4 filesystems..."
+    fstrim -Av "$1"
+}
+
+ext4_fsck() {
+    send_message "Ext4 Filesystem Check" "Performing ext4 filesystem check..."
+    e2fsck -f "$1"
+}
+
+xfs_check() {
+    send_message "XFS Check" "Performing XFS check..."
+    xfs_repair "$1"
+}
+
+fat32_maintenance() {
+    send_message "FAT32 Maintenance" "Performing FAT32 maintenance on $disk..."
+    dosfsck -a "$1"
+}
+
+zfs_maintenance() {
+
+    local pool_name
+    pool_name=$(zpool list -H -o name)
+    if [ -z "$pool_name" ]; then
+        echo "No ZFS pools found."
+        return 1
+    fi
+    for pool_name in $pool_name; do
+        send_message "ZFS Check" "Starting ZFS scrub on $pool_name..."
+        zpool scrub "$pool_name"
     done
 }
 
-# Function to perform btrfs balance
-function btrfs_balance {
-    send_message "Btrfs Balance" "Performing btrfs balance on $1..."
-    btrfs balance start -dusage=50 $1
+main() {
+    for disk in /dev/sd? /dev/nvme[0-9]n[0-9] /dev/mmcblk[0-9]p[0-9]; do
+
+        badblocks_check "$disk"
+        smart_test "$disk"
+ 
+        fs_type=$(blkid -o value -s TYPE "$disk")
+
+        fsck_check "$fs_type" "$disk"
+        fs_maintenance "$fs_type" "$disk"
+    done
+    clean_system_logs
 }
 
-# Function to perform ext4 filesystem check
-function ext4_fsck {
-    send_message "Ext4 Filesystem Check" "Performing ext4 filesystem check on $1..."
-    e2fsck -f $1
-}
-
-# Function to perform xfs filesystem check
-function xfs_check {
-    send_message "XFS Check" "Performing XFS check on $1..."
-    xfs_check $1
-}
-
-# Send start message
-send_message "HDD Scan Start" "Started a HDD scan on $HOSTNAME on the $(date)"
-
-# Perform checks
-perform_checks "regular hard drives" "/dev/sd?" "badblocks_check"
-perform_checks "regular hard drives" "/dev/sd?" "smart_test"
-perform_checks "NVMe disks" "/dev/nvme[0-9]n[0-9]" "smart_test"
-perform_checks "SD cards" "/dev/mmcblk[0-9]p[0-9]" "fsck_check"
-
-# Check filesystem state of ext4 and btrfs filesystems and schedule fsck if not clean
-df -T | awk '/ext4|btrfs/ {system(\"./check_filesystem_state \"$7)}'
-
-# Perform btrfs balance on btrfs filesystems
-df -T | awk '/btrfs/ {system(\"./btrfs_balance \"$7)}'
-
-# Perform ext4 filesystem check on ext4 filesystems
-df -T | awk '/ext4/ {system(\"./ext4_fsck \"$7)}'
-
-# Perform xfs filesystem check on xfs filesystems
-df -T | awk '/xfs/ {system(\"./xfs_check \"$7)}'
-
-# Find log files larger than 1k and send them
-find "$LOGS" -maxdepth 1 -type f -size +1k -exec send_file {} \\;
-
-# Clean system logs
-clean_system_logs
+main
