93be5b3c53daca134ea73e0c12e851e438ef512a
Toast <swetoast@users.noreply.github.com>
2024-07-28 15:12:33 +0200

    Update check_drives_v2.sh
    Not ready from prime time

--- harddrives/check_drives_v2.sh
+++ harddrives/check_drives_v2.sh
@@ -3,6 +3,9 @@
 
 CONFIG=/opt/etc/unattended_update.conf
 
+# Add a summary array to store the results
+declare -A summary
+
 if [ "$(id -u)" != "0" ]; then exec sudo /bin/bash "$0"; fi
 
 if [ ! -f "$CONFIG" ]; then
@@ -18,7 +21,9 @@ send_message() {
     local event=$1
     local body=$2
     local title="$HOSTNAME - $event"
-    curl -u "$pushbullet_token": https://api.pushbullet.com/v2/pushes -d type=note -d title="$title" -d body="$body"
+    # Accumulate messages instead of sending them immediately
+    # Use printf to interpret the newline character
+    summary["$title"]=$(printf "%s\n" "$body")
 }
 
 send_file() {
@@ -29,13 +34,15 @@ send_file() {
     local max_size_bytes=$((max_file_size * 1024 * 1024))
 
     if [ $(stat -c%s "$file_path") -le "$max_size_bytes" ]; then
-        curl -u "$pushbullet_token": https://api.pushbullet.com/v2/pushes -X POST -F type=file -F file_name="$file_name" -F file=@"$file_path" -F title="$title"
-    else
+        # Accumulate file path instead of file content
+        send_message "$title" "$file_path"
+    else 
         send_message "File Compression" "File $file_name is larger than $max_file_size MB. Attempting to compress..."
         gzip -c "$file_path" > "$file_path.gz"
         if [ $(stat -c%s "$file_path.gz") -le "$max_size_bytes" ]; then
-            curl -u "$pushbullet_token": https://api.pushbullet.com/v2/pushes -X POST -F type=file -F file_name="$file_name.gz" -F file=@"$file_path".gz -F title="$title"
-        else
+            # Accumulate file path instead of file content
+            send_message "$title" "$file_path.gz"
+        else 
             send_message "File Compression" "Compressed file $file_name.gz is still larger than $max_file_size MB and will not be sent."
         fi
     fi
@@ -45,15 +52,17 @@ run_command() {
     local command=$1
     local mountpoint=$2
     local message=$3
-    send_message "$message" "Starting $message on $mountpoint"
-    eval "$command" &
-    local pid=$!
-    while ps -p $pid > /dev/null; do sleep 1; done
-    if [ $? -ne 0 ]; then
+    # Capture the output of the command
+    local output
+    output=$(eval "$command")
+    local exit_status=$?
+    if [ $exit_status -ne 0 ]; then
         send_message "$message" "$message failed on $mountpoint"
-    else
+    else 
         send_message "$message" "$message completed on $mountpoint"
     fi
+    # Return the output of the command
+    echo "$output"
 }
 
 check_smart() {
@@ -64,7 +73,7 @@ check_smart() {
             nvme smart-log "$disk" > "$log_file"
             send_file "$log_file"
         fi
-    else
+    else 
         if smartctl -i "$disk" | grep -q -E "SMART support is: Available|SMART/Health Information"; then
             if ! smartctl -i "$disk" | grep -q "SMART support is: Enabled"; then
                 smartctl --smart=on --offlineauto=on --saveauto=on "$disk"
@@ -84,12 +93,16 @@ for partition_info in "${partition_array[@]}"; do
 
     case $fstype in
         btrfs)
-            run_command "btrfs scrub start -Bd $mountpoint" $mountpoint "Scrub Operation"
-            run_command "btrfs balance start -dusage=50 -musage=50 $mountpoint" $mountpoint "Balance Operation"
-            run_command "btrfs filesystem defragment $mountpoint" $mountpoint "Defragmentation Operation"
+            scrub_output=$(run_command "btrfs scrub start -Bd $mountpoint" $mountpoint "Scrub Operation")
+            send_message "Scrub Output on $mountpoint" "$scrub_output"
+            balance_output=$(run_command "btrfs balance start -dusage=50 -musage=50 $mountpoint" $mountpoint "Balance Operation")
+            send_message "Balance Output on $mountpoint" "$balance_output"
+            defrag_output=$(run_command "btrfs filesystem defragment $mountpoint" $mountpoint "Defragmentation Operation")
+            send_message "Defrag Output on $mountpoint" "$defrag_output"
             ;;
         ext4)
-            run_command "fsck -N $mountpoint" $mountpoint "File System Check"
+            fsck_output=$(run_command "fsck -N $mountpoint" $mountpoint "File System Check")
+            send_message "FSCK Output on $mountpoint" "$fsck_output"
             ;;
     esac
 done
@@ -98,3 +111,12 @@ disks=$(lsblk -dn -o NAME | grep -E '^(sd[a-z]|nvme[0-9]n[0-9])$')
 for disk in $disks; do
     check_smart "/dev/$disk"
 done
+
+# At the end of the script, send a summary message
+summary_message="Summary of operations:\n"
+for key in "${!summary[@]}"; do
+    # Use printf to preserve the newlines in each message
+    summary_message+=$(printf "%s:\n%s\n\n" "$key" "${summary[$key]}")
+done
+# Send the accumulated summary message
+curl -u "$pushbullet_token": https://api.pushbullet.com/v2/pushes -d type=note --data-urlencode title="Summary" --data-urlencode body="$summary_message"
